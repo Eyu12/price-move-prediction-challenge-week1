8ac3b75bcf0a626e97f71296f5ec3b9148bec51d
Eya Zewdu <eyazew1@gmail.com>
2025-11-21 22:28:06 +0300

    Update test data loading

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -1,5 +1,5 @@
 def test_data_columns():
     import pandas as pd
-    data = pd.read_csv("../data/raw_analyst_ratings.csv")
+    data = pd.read_csv("data/raw_analyst_ratings.csv")
     assert "headline" in data.columns
     assert "date" in data.columns
